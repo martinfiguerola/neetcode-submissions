1ef4b9122b4c8e6c22a77912aa0af23f4ea87a43
Martin Figuerola <81661747+martinfiguerola@users.noreply.github.com>
2026-07-18 12:44:34 +0200

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+    // 1 Create empty integer array ans with lenght of nums.leng * 2
+
+    int[] ans = new int[nums.length * 2];
+
+    // 2 PARA cada 'i' desde 0 hasta len(ans) - 1:
+    for (int i = 0; i < ans.length; i++) {
+        // SI i > nums.leng - 1
+        // ans[i] = nums[i - nums.leng]
+        if (i > nums.length - 1) ans[i] = nums[i - nums.length];
+        // SINO  ans[i] = nums[i]
+        else ans[i] = nums[i];
+
+    }   
+    // 3 RETURN ans
+    return ans;
+
+    }
+}
