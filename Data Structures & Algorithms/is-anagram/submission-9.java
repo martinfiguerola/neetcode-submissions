24d2a60ea59334f8e9dd5c1d8bb5dbbcc43749bf
Martin Figuerola <81661747+martinfiguerola@users.noreply.github.com>
2026-08-30 13:22:05 +0200

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) return false;
+            map.put(c, map.get(c) - 1);
+        }
+        return true;
+
+        // space O(n) and time O(n)
+    }
+}
