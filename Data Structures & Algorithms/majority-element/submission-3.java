8c230718652c8a243d31ddbb7cef12563c5f2d93
Martin Figuerola <81661747+martinfiguerola@users.noreply.github.com>
2026-09-03 12:16:56 +0200

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0, count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                res = num;
+                count = 1;
+            } 
+            else if (num == res) count++;
+            else count--;
+        }
+
+        return res;
+    }
+}
