be3a30f6b45df1422fdcc90703a95b412f42145b
Martin Figuerola <81661747+martinfiguerola@users.noreply.github.com>
2026-09-07 20:37:18 +0200

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs) {
+
+            char[] chars = str.toCharArray();
+
+            Arrays.sort(chars);
+            
+            String s = new String(chars);
+
+            map.putIfAbsent(s, new ArrayList<>());
+
+            map.get(s).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
