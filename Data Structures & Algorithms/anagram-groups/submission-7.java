488fa817a83be6687ef1949da3f3468d8624a51f
Martin Figuerola <81661747+martinfiguerola@users.noreply.github.com>
2026-09-11 10:36:51 +0200

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+
+            Arrays.sort(array);
+
+            String sorted = new String(array);
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+        
+    }
+}
