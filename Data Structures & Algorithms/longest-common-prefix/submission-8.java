e59e4afe19f44fcaedf9c982a5a892234d728590
Martin Figuerola <81661747+martinfiguerola@users.noreply.github.com>
2026-09-15 11:27:29 +0200

    Add: longest-common-prefix - submission-8

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-8.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 0; i < prefix.length(); i++){
+            for (String s : strs) {
+                if(i == s.length() || s.charAt(i) != prefix.charAt(i)){
+                    return prefix.substring(0, i); 
+                }
+            }
+        }  
+        return prefix;
+    }
+}
